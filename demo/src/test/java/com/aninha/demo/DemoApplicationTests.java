96aecf7f43d6d577a414ead9189f4961e390836c
Ana Laura Lopes <131914458+AnaLLopes@users.noreply.github.com>
2025-04-01 01:04:54 +0000

    Aula3103

---
+++ demo/src/test/java/com/aninha/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aninha.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
